99e7bb1d80d09a9706fca6ed41de91c75a41ecce
neonxflame <124432647+shermanTWQ@users.noreply.github.com>
2023-06-15 20:11:38 +0800

    Add files via upload

---
+++ EasyWiz/EasyWiz.vb
@@ -0,0 +1,11 @@
+﻿Public Class EasyWiz
+    Private Sub btnCheck_Click(sender As Object, e As EventArgs) Handles btnCheck.Click
+        TextBoxComputerName.Text = Environment.MachineName
+        TextBoxOSVersion.Text = System.Environment.OSVersion.ToString
+        TextBoxOSFullName.Text = My.Computer.Info.OSFullName
+        TextBoxCurrentUser.Text = System.Environment.UserName
+        TextBox64Bit.Text = System.Environment.Is64BitOperatingSystem
+        TextBoxSystemDirectory.Text = System.Environment.SystemDirectory
+        TextBoxDomain.Text = System.Environment.UserDomainName
+    End Sub
+End Class
---
+++ EasyWiz/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("EasyWiz")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("EasyWiz")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("21926b33-8670-42e0-be08-f3ceb9210536")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
